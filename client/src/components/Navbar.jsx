bd576066678278e635a0426c41c8e53463641cf0
PC <humzam241@outlook.com>
2026-08-28 16:11:28 -0400

    fix: tailor apparel photo colour masks

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ export default function Navbar() {
             <div className="w-8 h-8 rounded-lg bg-gray-900 flex items-center justify-center">
               <ShoppingBag className="w-4 h-4 text-white" />
             </div>
-            Apparel
+            Deed Leisure
           </Link>
 
           {/* Desktop nav */}
